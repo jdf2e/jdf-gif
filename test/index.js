e661bebc5440967e07a1b385a996e11a047da536
wshxbqq <wshxbqq@gmail.com>
2017-01-14 03:06:53 +0800

    add .

--- test/index.js
+++ test/index.js
@@ -1,10 +1,22 @@
 'use strict';
-var jdfGif = require("../");
+var gifsicle = require("../");
+var fs = require("fs");
 var execFile = require('child_process').execFile;
+var path = require("path");
 
-var child = execFile(jdfGif, ['--version'], function(error, stdout, stderr) {
+execFile(gifsicle, ['--version'], function(error, stdout, stderr) {
     if (error) {
         throw error;
     }
-    console.log(stdout);
+});
+
+var testPath = path.join(__dirname, "1_mini.gif");
+if (fs.existsSync(testPath)) {
+    fs.unlinkSync(testPath)
+}
+
+execFile(gifsicle, ['-o', testPath, path.join(__dirname, "1.gif")], error => {
+    if (error) {
+        throw error;
+    }
 });
